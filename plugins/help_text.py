c1c4f2508a66498fc1f62f211e97bbcff0423be6
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-21 14:04:24 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -21,4 +21,28 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-.
+@pyrogram.Client.on_message(pyrogram.filters.command(["start"]) & filters.private)
+async def start(bot, update):
+    await update.reply_text(
+        text=Script.START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=Script.START_BUTTONS
+    )
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["help"]) & filters.private)
+async def help(bot, update):
+    await update.reply_text(
+        text=Script.HELP_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Script.HELP_BUTTONS
+    )
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["about"]) & filters.private)
+async def about(bot, update):
+    await update.reply_text(
+        text=Script.ABOUT_TEXT,
+        disable_web_page_preview=True,
+        reply_markup=Script.ABOUT_BUTTONS
+    )
+
