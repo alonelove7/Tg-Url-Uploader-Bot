93e29c2e31d27acea7a9d1b8a52bc0ce076ebf40
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-21 12:56:34 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -21,12 +21,49 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@Client.on_message(filters.private & filters.incoming & filters.command("start"))
-async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
-	await bot.send_message(
-		msg.chat.id,
-		Script.START.format(msg.from_user.mention, mention),
-		reply_markup=InlineKeyboardMarkup(Script.buttons)
-	)
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+from helper_funcs.forcesub import ForceSub
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
+async def help_user(bot, update):
+    forcesub = await ForceSub(bot, update)
+    if forcesub == 400:
+        return
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Script.HELP_TEXT,
+            parse_mode="html",
+            disable_web_page_preview=True,
+            reply_to_message_id=update.message_id
+        )
+
+@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
+async def start(bot, update):
+    #forcesub = await ForceSub(bot, update)
+    #if forcesub == 400:
+        #return
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=Script.START_TEXT.format(update.from_user.first_name),
+            reply_markup=InlineKeyboardMarkup
+
