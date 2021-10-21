7f2943fcdd934e198212b2909acaee4b39b061d2
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-21 12:46:18 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -21,38 +21,3 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram.types.bots_and_keyboards import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-@pyrogram.Client.on_message(pyrogram.filters.command(["help"]))
-async def help_user(bot, update):
-    forcesub = await ForceSub(bot, update)
-    if forcesub == 400:
-        return
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.HELP_USER,
-            parse_mode="html",
-            disable_web_page_preview=True,
-            reply_to_message_id=update.message_id
-        )
-
-
-@pyrogram.Client.on_message(pyrogram.filters.command(["start"]))
-async def start(bot, update):
-    #forcesub = await ForceSub(bot, update)
-    #if forcesub == 400:
-        #return
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.START_TEXT.format(update.from_user.first_name),
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            "sᴜᴘᴘᴏʀᴛ", url="https://t.me/TeleRoid14"
-                        ),
-                        InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/TeleRoidGroup"),
-                    ],
-                    [InlineKeyboardButton("ᴅᴇᴠᴇʟᴏᴘᴇʀ", url="https://t.me/TheTeleRoid")],
-                ]
-            ),
-            reply_to_message_id=update.message_id
-        )
